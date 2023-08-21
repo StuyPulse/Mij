94f974e221addc50ae55ffdd058e286282e1a381
BenG49 <ben@goldfisher.com>
2023-08-21 22:25:01 +0800

    Add preliminary motor ports

--- src/main/java/com/stuypulse/robot/constants/Ports.java
+++ src/main/java/com/stuypulse/robot/constants/Ports.java
@@ -12,4 +12,44 @@ public interface Ports {
         int OPERATOR = 1;
         int DEBUGGER = 2;
     }
+
+    public interface Swerve {
+        public interface FrontRight {
+            int DRIVE = 10;
+            int TURN = 11;
+        }
+
+        public interface FrontLeft {
+            int DRIVE = 12;
+            int TURN = 13;
+        }
+        
+        public interface BackLeft {
+            int DRIVE = 14;
+            int TURN = 15;
+        }
+        
+        public interface BackRight {
+            int DRIVE = 16;
+            int TURN = 17;
+        }
+    }
+
+    public interface Arm {
+        int LEFT = 20;
+        int RIGHT = 21;
+    }
+
+    public interface Elevator {
+        int LEFT = 30;
+        int RIGHT = 31;
+    }
+
+    public interface Wrist {
+        int MOTOR = 40;
+    }
+
+    public interface Intake {
+        int MOTOR = 50;
+    }
 }
