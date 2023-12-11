5c87bfd0629f8fd8aef872b561f58afe1aa97143
Kalimul Kaif <kalimul.kaif@stuypulse.com>
2023-12-11 17:01:28 -0500

    add set zero mode keybinding

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -24,6 +24,7 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class RobotContainer {
 
@@ -66,6 +67,8 @@ public class RobotContainer {
         driver.getDPadDown().onTrue(new SwerveDriveResetHeading(Rotation2d.fromDegrees(180)));
         driver.getDPadLeft().onTrue(new SwerveDriveResetHeading(Rotation2d.fromDegrees(90)));
         driver.getDPadRight().onTrue(new SwerveDriveResetHeading(Rotation2d.fromDegrees(270)));
+
+        driver.getLeftButton().onTrue(new InstantCommand(SwerveDrive.getInstance()::setZeroMode));
     }
 
     /**************/
